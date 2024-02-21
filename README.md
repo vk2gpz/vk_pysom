7eb861b3b57223dcd1011e67c7aa57e3f9089206
vk2gpz <vk2gpz@gmail.com>
2024-02-22 10:29:11 +1100

    initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# gpz_som
+This is a collection of Self-Organizing Map geometry related python packages
+
+# License
+This software is released under the condition described in the LICENSE.
+
+## install
+pip install git+ssh://git@github.com/vk2gpz/vk_som.git
+
+notice that ':' in "pip install git+ssh://git@github.com:vk2gpz/vk_som.git" needs to be changed to '/'
+
+## test
+python
+```
+>>> import vk.geom.grid.som as som
+>>> som.main()
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="vk_som",
+    version="1.0.0",
+    author="vk2gpz",
+    author_email="vk2gpz@gmail.com",
+    license='LGPL',
+    description="A collection of Self-Organizing Map related modules.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vk2gpz/vk_som",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_namespace_packages(include=['vk_som.*']),
+    python_requires=">=3.9",
+    install_requires=[
+          'vk_geom @ git+ssh://git@github.com/vk2gpz/vk_geom@main',
+      ],
+)
