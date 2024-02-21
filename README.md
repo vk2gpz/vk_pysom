46514f125a3754d9b7a9322614358b5e523b1c7c
vk2gpz <vk2gpz@gmail.com>
2024-02-22 10:34:13 +1100

    renamed vk_som to vk_pysom

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# vk_som
+# vk_pysom
 This is a collection of Self-Organizing Map geometry related python packages
 
 # License
 This software is released under the condition described in the LICENSE.
 
 ## install
-pip install git+ssh://git@github.com/vk2gpz/vk_som.git
+pip install git+ssh://git@github.com/vk2gpz/vk_pysom.git
 
-notice that ':' in "pip install git+ssh://git@github.com:vk2gpz/vk_som.git" needs to be changed to '/'
+notice that ':' in "pip install git+ssh://git@github.com:vk2gpz/vk_pysom.git" needs to be changed to '/'
 
 ## test
 python
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="vk_som",
+    name="vk_pysom",
     version="1.0.0",
     author="vk2gpz",
     author_email="vk2gpz@gmail.com",
@@ -12,15 +12,15 @@ setuptools.setup(
     description="A collection of Self-Organizing Map related modules.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/vk2gpz/vk_som",
+    url="https://github.com/vk2gpz/vk_pysom",
     classifiers=[
         "Programming Language :: Python :: 3",
         # "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=setuptools.find_namespace_packages(include=['vk_som.*']),
+    packages=setuptools.find_namespace_packages(include=['vk_pysom.*']),
     python_requires=">=3.9",
     install_requires=[
-          'vk_geom @ git+ssh://git@github.com/vk2gpz/vk_geom@main',
+          'vk_pygeom @ git+ssh://git@github.com/vk2gpz/vk_pygeom@main',
       ],
 )
